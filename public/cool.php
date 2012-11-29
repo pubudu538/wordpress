e29b51df3ea658116d5eeec03440e2a30427af72
Lakmal Warusawithana <lakmal@wso2.com>
2012-11-29 13:27:30 +0530

    s

---
+++ public/cool.php
@@ -0,0 +1,7 @@
+<?php
+
+session_start();
+
+$_SESSION['ss']="11111111";
+header('Location: kool.php');
+?>
---
+++ public/kool.php
@@ -0,0 +1,7 @@
+<?php
+
+session_start();
+echo ">>>>>>>>>>>>>>";
+print_r($_SESSION);
+echo "<<<<<<<<<<<<";
+?>
