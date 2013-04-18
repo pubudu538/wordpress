5134e722adbfc57ad3a30b313004359722b12cd5
Lakmal <lakmal@lakmal-ThinkPad-T410.(none)>
2013-04-18 16:43:50 +0530

    test

--- www/cool.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-session_start();
-
-$_SESSION['ss']="11111111";
-header('Location: kool.php');
-?>
--- www/kool.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-session_start();
-echo ">>>>>>>>>>>>>>>>";
-print_r($_SESSION);
-echo "<<<<<<<<<<<<<";
-?>
